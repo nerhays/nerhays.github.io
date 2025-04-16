78edc99f45d17631a93e5ed32715545943d50dbe
nerhays <sfa.asus@gmail.com>
2025-04-16 19:11:31 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function swapImages() {
+  const img1 = document.getElementById("img1");
+  const img2 = document.getElementById("img2");
+
+  // Swap z-index and transform
+  if (img1.classList.contains("front")) {
+    img1.classList.remove("front");
+    img2.classList.add("front");
+  } else {
+    img2.classList.remove("front");
+    img1.classList.add("front");
+  }
+}
+
+function handleLearnMore() {
+  window.open("https://youtu.be/FQgYyLKP6y4?si=LOEMqis_p9T-Q4ZT", "_blank");
+}
+
+document.querySelectorAll(".footer-nav a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute("href").substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
